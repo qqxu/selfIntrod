ba9958b6c8c4e4c4ead738120c0214f81e46128f
qqxu <qqxu10@126.com>
2016-08-03 21:54:11 +0800

    sele introduction
    sele introduction

---
+++ js/toolJs.js
@@ -0,0 +1,16 @@
+﻿		/*倒计时*/
+		function showTime(){
+			var endTime=new Date("2017/7/1,0:0:0");//毕业时间
+			var nowTime=new Date();//当前时间
+			var leftTime=parseInt((endTime.getTime()-nowTime.getTime())/1000); //剩下时间（秒）
+			var d=parseInt(leftTime/(24*60*60));
+			var h=parseInt(leftTime/(60*60)%24);
+			var m=parseInt(leftTime/60%60);
+			var s=parseInt(leftTime%60);
+			document.getElementById("leftTime").innerHTML="距离毕业剩余"+d+"天"+h+"时"+m+"分"+s+"秒";
+			if(leftTime<=0){
+					document.getElementById("leftTime").innerHTML="毕业啦!"
+			}
+			setTimeout(showTime,500);
+
+		}
